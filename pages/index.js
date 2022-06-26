65d3287facc7f967d2874ab84db655806a112717
Iqbal Hossan <engiqbal1999@gmail.com>
2022-06-26 16:30:12 +0600

    get pizza from database and display on home page

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,10 @@
+import axios from "axios";
 import Head from "next/head";
 import PizzaList from "../component/PizzaList";
 import Slider from "../component/Slider";
 import styles from "../styles/Home.module.css";
 
-export default function Home() {
+export default function Home({ pizzas }) {
   return (
     <div className={styles.container}>
       <Head>
@@ -12,7 +13,16 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Slider />
-      <PizzaList />
+      <PizzaList pizzas={pizzas} />
     </div>
   );
 }
+
+export const getServerSideProps = async () => {
+  const res = await axios.get("http://localhost:3000/api/products");
+  return {
+    props: {
+      pizzas: res.data,
+    },
+  };
+};
